d598d586e9685a36cfeff937316354d0b8dfe66a
User <User@DESKTOP-4ROMB9B>
2023-06-26 19:23:56 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task__1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            
+
+            Console.WriteLine("Введите целое число: ");
+            int x = int.Parse(Console.ReadLine());
+            
+
+
+            if (x % 2 == 0) 
+            {
+                Console.WriteLine($"Введенное число {x} является четным");
+            
+            }
+            else
+            {
+                Console.WriteLine($"Введенное число {x} является нечетным");
+                
+            }
+            Console.ReadKey();
+        }
+
+
+        
+        
+    }
+}
